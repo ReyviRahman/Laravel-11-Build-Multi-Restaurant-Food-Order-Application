1840b204fe13ac02f8508f7faeb895c770adcf3d
Reyvi Rahman <reyvirahman@gmail.com>
2025-09-21 13:08:02 +0700

    Add User Profile Dashboard

--- resources/views/frontend/index/layouts/header_home.blade.php
+++ resources/views/frontend/index/layouts/header_home.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light osahan-nav shadow-sm">
+<nav class="navbar navbar-expand-lg navbar-dark osahan-nav">
   <div class="container">
-    <a class="navbar-brand" href="index.html"><img alt="logo" src="img/logo.png"></a>
+    <a class="navbar-brand" href="index.html"><img alt="logo" src="img/favicon.png"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
       aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -12,7 +12,7 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="offers.html"><i class="icofont-sale-discount"></i> Offers <span
-              class="badge badge-danger">New</span></a>
+              class="badge badge-warning">New</span></a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
